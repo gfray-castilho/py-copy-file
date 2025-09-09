31e7a61b278bbc799c17caed2ffa8a5a8b30b898
Gabriel Fray <gfraycastilho@gmail.com>
2025-09-09 00:39:24 -0300

    SOLVED

--- app/main.py
+++ app/main.py
@@ -1,1 +1,19 @@
-# write your code here
+def copy_file(command: str):
+    if command.count(" ") != 2:
+        return
+
+    tokens = command.split(" ")
+    if tokens[0] != "cp":
+        return
+
+    source_file_name = tokens[1]
+    destination_file_name = tokens[2]
+
+    if source_file_name == destination_file_name:
+        return
+
+    try:
+        with open(source_file_name, "r") as file_in, open(destination_file_name, "w") as file_out:
+            file_out.write(file_in.read())
+    except FileNotFoundError:
+        pass
